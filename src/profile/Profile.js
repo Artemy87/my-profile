49b14d9c6f3b3c41616738397a6171ff6ccb6ff3
Artemy <artemij87work@gmail.com>
2022-06-02 13:16:57 +0300

    upd

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -5,20 +5,22 @@ import image from './image/IMG_8373.JPG';
 export const Profile = () => {
 	return (
 		<div className={styles.profile}>
-			<div className={styles.container}>
-				<div>
-					<img alt="avatar" className={styles.image} src={image}/>
+			<div className={styles.profile2}>
+				<div className={styles.container}>
+					<div>
+						<img alt="avatar" className={styles.image} src={image}/>
+					</div>
+					<div className={styles.textHi}>
+						Привет, я
+						<span className={styles.textName}>Артемий</span>
+					</div>
+					<div className={styles.textRD}>React разработчик.</div>
+					<div className={styles.description}>Пару слов о моих навыках будут описаны здесь</div>
 				</div>
-				<div className={styles.textHi}>
-					Привет, я
-					<span className={styles.textName}>Артемий</span>
+				<div className={styles.buttons}>
+					<button>Download My CV</button>
+					<button>Hire Me</button>
 				</div>
-				<div className={styles.textRD}>React разработчик.</div>
-				<div className={styles.description}>Пару слов о моих навыках будут описаны здесь</div>
-			</div>
-			<div className={styles.buttons}>
-				<button>Download My CV</button>
-				<button>Hire Me</button>
 			</div>
 		</div>
 	);
