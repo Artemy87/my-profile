4a3c059049f671739e369c077b79fdaec6e3605a
Artemy <artemij87work@gmail.com>
2022-06-20 21:36:17 +0300

    upd mobile version

--- src/App.js
+++ src/App.js
@@ -10,12 +10,9 @@ export default function App() {
 
 	const skills = ['JavaScript', 'TypeScript', 'React', 'Redux', 'Redux-Thunk', 'Material UI', 'Unit Tests']
 	const projects = [
-		{id: 1, project: 'project1', description: 'My first project'},
-		{id: 2, project: 'project2', description: 'Todolist'},
-		{
-			id: 3,
-			project: 'project3',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolore iusto laborum modi quia quibusdam quod tempore totam. Accusamus commodi dolorum id laudantium libero mollitia nisi, quasi quia rem sequi! Corporis dolorem dolores est et fugit id inventore iure numquam odio omnis porro, quo, repellat, sapiente sunt totam velit voluptates. Ad aperiam, eius eveniet fuga ipsum magni perspiciatis qui voluptates.'
+		{id: 1, project: 'project1', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolore iusto laborum modi quia quibusdam quod tempore totam. Accusamus commodi dolorum id laudantium.'},
+		{id: 2, project: 'project2', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolore iusto laborum modi quia quibusdam quod tempore totam. Accusamus commodi dolorum id laudantium libero mollitia nisi, quasi quia rem sequi! Corporis dolorem dolores est.'},
+		{id: 3, project: 'project3', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolore iusto laborum modi quia quibusdam quod tempore totam. Accusamus commodi dolorum id laudantium libero mollitia nisi, quasi quia rem sequi! Corporis dolorem dolores est et fugit id inventore iure numquam odio omnis porro, quo, repellat, sapiente sunt totam velit voluptates.'
 		}
 	];
 
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -6,17 +6,15 @@ export const Profile = () => {
 	return (
 		<div className={styles.profile}>
 			<div className={styles.profile2}>
-				<div className={styles.container}>
-					<div>
-						<img alt="avatar" className={styles.image} src={image}/>
-					</div>
-					<div className={styles.textHi}>
-						Привет, я
-						<span className={styles.textName}>Артемий</span>
-					</div>
-					<div className={styles.textRD}>React разработчик.</div>
-					<div className={styles.description}>Пару слов о моих навыках будут описаны здесь</div>
+				<div>
+					<img alt="avatar" className={styles.image} src={image}/>
 				</div>
+				<div className={styles.textHi}>
+					Привет, я
+					<span className={styles.textName}>Артемий</span>
+				</div>
+				<div className={styles.textRD}>React разработчик.</div>
+				<div className={styles.description}>Пару слов о моих навыках будут описаны здесь</div>
 				<div className={styles.buttons}>
 					<button>Download My CV</button>
 					<button>Hire Me</button>
